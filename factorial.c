6cfeb6ae5e30e0ecc42137fd5f1467e2e3aed434
Swapnil7000 <53923373+Swapnil7000@users.noreply.github.com>
2019-09-21 07:06:44 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -6,7 +6,8 @@ int main()
  while(i!=0)//loop to multiply digit till 1 at 0 condition will be false and factorial will be printed
  {
  	fact=fact*i;//fact=1*i then i will be stored in fact next time i*i-1 will run after whole process at the end i*(i-1)*.....1
- 	i--;//to decreament i by 1 }
+ 	i--;//to decreament i by 1 
+ }
  
  printf("%d",fact);//to print factorial of the number
 }
