ad858151309791b09b89b2f018f545eb7ecfacef
Swapnil bhojwani <53923373+Swapnil7000@users.noreply.github.com>
2019-10-03 16:50:05 +0530

    Update Pattern2.c

--- Pattern2.c
+++ Pattern2.c
@@ -3,34 +3,31 @@
 int main()
 {
 
-int i,j=3,k,n;
+int i,j=3,k,n;//Decleration of variables n is number of rows
 scanf("%d",&n);
-for(i=1;i<=n;i++)
-
-{   
-	for(k=1;k<=i;k++)
-	{     
-			printf("%d",j);
-		}
-		
-	j++;
-	printf("\n");}
-	j--;
-	for(i=3;i>=1;i--)
-
-{   
-	for(k=i;k>=1;k--)
-	{     
-			printf("%d",j);
-		}
-		
-	j--;
-	printf("\n");}
+for(i=1;i<=n;i++)//Looping till i less than n
+{  
+	for(k=1;k<=i;k++)//Loop to print j
+	{ 
+		printf("%d",j);
 	}
+	j++;//Post increamenting j till k<=i
+	printf("\n");}
+	j--;//Post decrement to print j in decreasing order
+	for(i=n;i>=1;i--)//Looping till i is greater than one
+	{   
+	for(k=i;k>=1;k--)//Loop to print j
+	{     
+	printf("%d",j);
+	}
+	j--;//Post drecement
+	printf("\n");//To move into new line}
+}
 	
 /*  Output:-  3
               44
               555
               555
               44
-              3 */
+              3
+	          if n=3*/
