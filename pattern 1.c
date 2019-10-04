b4c8614e3ece0343db1573e9ee04e7de36e66d26
akshayakshay30 <55040814+akshayakshay30@users.noreply.github.com>
2019-10-04 11:15:56 +0530

    Update pattern 1.c

--- pattern 1.c
+++ pattern 1.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,n;
+        int n;
 	printf("Enter the number of rows\n");
 	scanf("%d",&n);
-	for(i=0;i<=n;i++)
+	for(int i=0;i<=n;i++)
 	{
-		for(j=0;j<=i;j++)
+		for(int j=0;j<=i;j++)
 		{
 			printf("*");
 		}
-		printf("\n");}
+		printf("\n");
+        }
 }
 /*  *
     **
